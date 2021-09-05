5c31567dca4832945295687418dd235cf59ae990
ArturKandyba <artur.kandyba.git@gmail.com>
2021-09-05 19:53:53 +0300

    hw-08-in-progress-6

--- js/modal.js
+++ js/modal.js
@@ -9,12 +9,8 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open");
     refs.modal.classList.toggle('is-hidden');
   }
 })();
 
-(() => {
-  function toggleModal() {
-    document.body.classList.toggle("modal-open");
-  }
-})();
